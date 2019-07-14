945c93ac5ecd01f85cd1cafd5adafcb80f0a0500
Belle P <bellepx3@gmail.com>
2019-07-14 05:12:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
 
   if user_input == random_number.to_s
     puts "You guessed the correct number!"
-  elsif user_input == "exit"
+  elsif user_input.downcase == "exit"
      puts "Goodbye!"
    else
      puts "Sorry! the computer guessed #{random_number}." 
